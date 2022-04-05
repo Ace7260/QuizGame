1e385606cb971a13eb8cbf223ccac7026a5ca3f7
Ace7260 <irano7260@gmail.com>
2022-04-05 15:41:58 +0200

    add question on UI

--- app/src/main/java/com/example/shikroot/Model/QuestionBank.java
+++ app/src/main/java/com/example/shikroot/Model/QuestionBank.java
@@ -1,2 +1,26 @@
-package com.example.shikroot.Model;public class QuestionBank {
+package com.example.shikroot.Model;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
+public class QuestionBank implements Serializable {
+    private List<Quiz> mQuestionList;
+    private int mQuestionIndex;
+
+
+    public QuestionBank(List<Quiz> questionList) {
+        mQuestionList = questionList;
+
+        Collections.shuffle(mQuestionList);
+    }
+
+    public Quiz getCurrentQuestion() {
+        return mQuestionList.get(mQuestionIndex);
+    }
+
+    public Quiz getNextQuestion() {
+        mQuestionIndex++;
+        return getCurrentQuestion();
+    }
 }
--- app/src/main/java/com/example/shikroot/Model/User.java
+++ app/src/main/java/com/example/shikroot/Model/User.java
@@ -1,2 +1,13 @@
-package com.example.shikroot.Model;public class User {
+package com.example.shikroot.Model;
+
+public class User {
+    private String mFirstName;
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        mFirstName = firstName;
+    }
 }
